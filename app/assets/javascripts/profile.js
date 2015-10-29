9d6194687f55b9b7ae0f6f6bd16f0bc45510c4a2
April Koh <aprilkoh@Aprils-MBP.wireless.yale.internal>
2015-10-29 03:25:54 -0400

    made nav bar opaque, fixed the profile picture on the left

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -18,3 +18,21 @@ $(document).ready(function(){
     nextArrow: $('.carousel-next-2')
   });
 });
+
+$(document).ready(function(){
+  $('.slick-carousel-3').slick({
+    slidesPerRow: 5,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    nextArrow: $('.carousel-next-3')
+  });
+});
+
+$(document).ready(function(){
+  $('.slick-carousel-4').slick({
+    slidesPerRow: 7,
+    slidesToShow: 7,
+    slidesToScroll: 7,
+    nextArrow: $('.carousel-next-4')
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'profile/index'
+  get 'aprilkoh' => 'profile#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
