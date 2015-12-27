c8a2677f9499df94d4b567242b2847c2be392a40
April <april.koh@yale.edu>
2015-12-26 19:46:54 -0500

    removed admin boolean from users and made Admin model instead

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :books
   devise_for :users,
     :controllers => {:registrations => "users/registrations"}
-
+  devise_for :admins
   resources :categories
   resources :movies
   resources :users
