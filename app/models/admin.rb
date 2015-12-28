d964c819283097f315b70b212cd2f60b45f4af61
April <april.koh@yale.edu>
2015-12-27 21:28:32 -0500

    testing prod routes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,10 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  if Rails.env.production?
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  else
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
+  end
+
 end
