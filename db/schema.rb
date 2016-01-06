073ed8da1d0c2f8a6bd199dd5ca9969f0f988254
April <april.koh@yale.edu>
2016-01-06 04:49:27 -0500

    added featured boolean to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106025307) do
+ActiveRecord::Schema.define(version: 20160106094626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -208,6 +208,7 @@ ActiveRecord::Schema.define(version: 20160106025307) do
     t.string   "facebook"
     t.string   "twitter"
     t.string   "pinterest"
+    t.boolean  "featured"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
